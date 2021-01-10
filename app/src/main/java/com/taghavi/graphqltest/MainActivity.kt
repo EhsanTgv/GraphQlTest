44bf8ab2ab16c3158032c32724689a3884b4993b
EhsanTgv <EhsanTgv@gmail.com>
2021-01-10 20:44:16 +0330

    try another method

--- app/src/main/java/com/taghavi/graphqltest/MainActivity.kt
+++ app/src/main/java/com/taghavi/graphqltest/MainActivity.kt
@@ -2,38 +2,43 @@ package com.taghavi.graphqltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
-    private val BASE_URL = "https://api.github.com/graphql"
-    private lateinit var client: ApolloClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        client = setupApollo()
-    }
+        val scope = MainScope()
 
-    private fun setupApollo(): ApolloClient {
-        val okHttp = OkHttpClient
-            .Builder()
-            .addInterceptor { chain ->
-                val original = chain.request()
-                val builder = original.newBuilder().method(
-                    original.method(),
-                    original.body()
-                )
-                builder.addHeader(
-                    "Authorization", "Bearer "
-                )
-                chain.proceed(builder.build())
+        val apolloClient = ApolloClient.builder()
+            .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
+            .build()
+
+        scope.launch {
+            val response = try {
+                apolloClient.query(LaunchDetails(id = "83")).toDeferred().await()
+            } catch (e: ApolloException) {
+                // handle protocol errors
+                return@launch
             }
-            .build()
-        return ApolloClient.builder()
-            .serverUrl(BASE_URL)
-            .okHttpClient(okHttp)
-            .build()
+
+            val launch = response.data?.launch
+            if (launch == null || response.hasErrors()) {
+                // handle application errors
+                return@launch
+            }
+
+            // launch now contains a typesafe model of your data
+            println("Launch site: ${launch.site}")
+        }
     }
 }
